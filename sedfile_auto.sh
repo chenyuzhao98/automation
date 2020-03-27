255870b1f74d64dfcf8de7f45571a218c5b8c511
Chenyu Zhao <chenyuzhao@Chenyus-MacBook-Pro.local>
2020-03-26 21:26:57 -0500

    Add test cases

--- sedfile_auto.sh
+++ sedfile_auto.sh
@@ -48,8 +48,31 @@ sed "$s" $3 > $4
 # # change the fourth argument
 # echo 's/\(.*'$1'\)\([^'$6']*'$6'\)\([^'$6']*'$6'\)\([^'$6']*'$6'\)[^'$6']*\('$6'.*\)/\1\2\3\4'$2'\5/'
 
+####### please refer to netlist.log and duplicate.txt as template files for the following tests
 # # standard input using " " as seperator
+# # before:		aele A = write_var("specs.txt" "W" "! Freq dB(S21) dB(S22) NF(2)" "\t");
+# # after:		aele A = write_var("first!" "W" "! Freq dB(S21) dB(S22) NF(2)" "\t");
 # ./sedfile_auto.sh 'var(' 'first!' duplicate.txt newdup.txt 1 " "
 
 # # standard input using "," as seperator with double quotes '"'
+# # before: 	aele A = write_var(specs.txt,W,! Freq dB(S21) dB(S22) NF(2),\t);
+# # after:		aele A = write_var("spec_1.txt",W,! Freq dB(S21) dB(S22) NF(2),\t);
 # ./sedfile_auto.sh 'var(' '"second!"' netlist.log newnet.txt 2 ","
+
+# # before: 	MLIN2:TL18  N__4 N__1 Subst="MSub1" W=10 um L=FET1_L3 um Wall1=2.5e+025 meter Wall2=2.5e+025 meter Mod=1 
+# # after: 		MLIN2:TL18  N__999 N__1 Subst="MSub1" W=10 um L=FET1_L3 um Wall1=2.5e+025 meter Wall2=2.5e+025 meter Mod=1 
+# ./sedfile_auto.sh 'MLIN2:TL18  ' 'N__999' netlist.log newnet.txt 1 " "
+
+# # before: 	MLOC2:TL3  N__0 Subst="MSub1" W=30 um L=3270 um Wall1=2.5e+025 meter Wall2=2.5e+025 meter Mod=1 
+# # after: 		MLOC2:TL3  N__0 Subst="MSub1" W=999 um L=3270 um Wall1=2.5e+025 meter Wall2=2.5e+025 meter Mod=1 
+# ./sedfile_auto.sh 'MLOC2:TL3  ' 'W=999' netlist.log newnet.txt 3 " "
+
+# # before: 	S2PMDIF:S4  N__11 0 N__10 0 File="fettm_noise.mdf" Type="s2pmdif" InterpMode="linear" InterpDom="ri" ExtrapMode="interpMode" CheckPassivity=0 iVar1=SAMPLE iVal1=sample iVar2="Vds" iVal2=4.0 iVar3="Ids" iVal3=0.02
+# # after:  	S2PMDIF:S4  N__11 0 N__10 0 File="fettm_noise.mdf" Type="CHANGED" InterpMode="linear" InterpDom="ri" ExtrapMode="interpMode" CheckPassivity=0 iVar1=SAMPLE iVal1=sample iVar2="Vds" iVal2=4.0 iVar3="Ids" iVal3=0.02
+# ./sedfile_auto.sh 'S2PMDIF:S4  ' 'Type="CHANGED"' netlist.log newnet.txt 6 " "
+
+# # before: 	FET2_L1=261.487 noopt{ 10 to 550 }
+# # after: 		FET2_L1=99.99 noopt{ 10 to 550 }
+# ./sedfile_auto.sh 'FET2_L1=' '99.99' netlist.log newnet.txt 1 " "
+
+
